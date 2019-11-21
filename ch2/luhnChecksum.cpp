61089e51fa15ccb0c3592da308fbbffc57732452
Nick <ncommella@gmail.com>
2019-11-20 16:38:28 -0800

    luhn sum logic working in numberReading()

--- ch2/luhnChecksum.cpp
+++ ch2/luhnChecksum.cpp
@@ -20,17 +20,34 @@ int doubleDigitValue (int digit) {
 
 void numberReading () {
 	string numString;
+	bool isEvenLength;
+	int luhnSum = 0;
 	cout << "Enter a number of an arbitrary length: " << "\n";
 	cin >> numString;
+
+	//determine if length of the number is even or odd
+	if (numString.length() % 2 == 0)
+		isEvenLength = true;
+	else
+		isEvenLength = false;
+
+	//convert number chars to number ints while going from right to left
+	//TODO: separate this into a function
 	for (int i = numString.length() - 1; i >= 0; i--) {
-		int curDigit = int(numString[i]) - 48; // subtracts difference in character codes to get accurate number in integerr form
-		cout << "I: " << i << "\n";
-		cout << curDigit << " - Sum of doubled digits = " << doubleDigitValue(curDigit) << "\n"; // sends each digit to doubleDigitValue() as they are read from right to left
+		int curDigit = int(numString[i]) - 48;
+		if (isEvenLength)
+			if (i % 2 == 0)
+				luhnSum += doubleDigitValue(curDigit);
+			else
+				luhnSum += curDigit;
+		else if (i % 2 == 0)
+			luhnSum += curDigit;
+		else
+			luhnSum += doubleDigitValue(curDigit);
+		//cout << "I: " << i << "\n";
+		//this sends, need to determine which to send to double
+		//cout << curDigit << " - Sum of doubled digits = " << doubleDigitValue(curDigit) << "\n"; // sends each digit to doubleDigitValue() as they are read from right to left
 
 	}
-	
-}
-
-void whichDouble () {
-	
+	cout << "The Luhn Sum of " << numString << " is " << luhnSum << "\n";
 }
