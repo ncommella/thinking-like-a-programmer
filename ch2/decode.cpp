d7ef324a1a4e75bb82fc65d587beaf8cc7776c4a
Nick <ncommella@gmail.com>
2020-04-14 19:23:58 -0700

    One int var in decode

--- ch2/decode.cpp
+++ ch2/decode.cpp
@@ -6,22 +6,19 @@ int main ()
 	cout << "Enter a three or four-digit number: ";
 	
 	char digitChar = cin.get();
-	int threeDigitNumber = (digitChar - '0') * 100;
-	int fourDigitNumber	= (digitChar - '0') * 1000;
+	int number = (digitChar - '0') * 100;
 	
 	digitChar = cin.get();
-	threeDigitNumber += (digitChar - '0') * 10;
-	fourDigitNumber += (digitChar - '0') * 100;
+	number += (digitChar - '0') * 10;
 
 	digitChar = cin.get();
-	threeDigitNumber += (digitChar - '0');
-	fourDigitNumber += (digitChar - '0') * 10;
+	number += (digitChar - '0');
 
 	digitChar = cin.get();
 	if (digitChar == 10) {
-		cout << "Three-digit number is: " << threeDigitNumber << "\n";
+		cout << "The number is: " << number << "\n";
 	} else {
-		fourDigitNumber += (digitChar - '0');
-		cout << "Four-digit number is: " << fourDigitNumber << "\n";
+		number = (number *10) + (digitChar - '0');
+		cout << "The number is: " << number << "\n";
 	}
 }
